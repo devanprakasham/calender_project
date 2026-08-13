877efa8720d2a42233f57ed96aaf210f86af7f80
Devan <devanprakasham@gmail.com>
2026-08-13 19:33:01 +0530

    Initial Calendar Flutter app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:calender_project/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget( CalendarApp());
+    await tester.pumpWidget(const CalendarApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
